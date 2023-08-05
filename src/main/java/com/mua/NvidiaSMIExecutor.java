1e9ddbb77b872b44fd0a14f811711c3dfea76901
maifeeulasad <maifeeulasad@gmail.com>
2023-08-05 21:05:42 +0600

    [nvidia]: using `nvidia-smi -q -x` to read data in XML format, and parsing it into JSON;

---
+++ src/main/java/com/mua/NvidiaSMIExecutor.java
@@ -0,0 +1,44 @@
+package com.mua;
+
+import org.json.JSONObject;
+import org.json.XML;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class NvidiaSMIExecutor {
+
+    public static void executeNvidiaSMI(Callback callback) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("nvidia-smi", "-q", "-x");
+            Process process = processBuilder.start();
+
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line);
+            }
+
+            int exitCode = process.waitFor();
+
+            if (exitCode != 0) {
+                callback.onError("nvidia-smi command failed with exit code: " + exitCode);
+            } else {
+                JSONObject jsonData = XML.toJSONObject(output.toString());
+
+                callback.onSuccess(jsonData);
+            }
+        } catch (Exception e) {
+            callback.onError(e.getMessage());
+        }
+    }
+
+    interface Callback {
+        void onSuccess(JSONObject data);
+
+        void onError(String errorMessage);
+    }
+}
