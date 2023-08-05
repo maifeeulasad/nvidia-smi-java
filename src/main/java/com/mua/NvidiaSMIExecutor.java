4d7154287f94e983cc44039368748fb0bf2d4c31
maifeeulasad <maifeeulasad@gmail.com>
2023-08-05 21:46:44 +0600

    [doc]: added javadoc into `NvidiaSMIExecutor`;

--- src/main/java/com/mua/NvidiaSMIExecutor.java
+++ src/main/java/com/mua/NvidiaSMIExecutor.java
@@ -7,8 +7,22 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * A utility class to execute the 'nvidia-smi' command and retrieve NVIDIA System Management Interface (nvidia-smi) data.
+ * The data is parsed from XML format to JSONObject format using the 'org.json' library.
+ * This class provides two methods for execution:
+ * - executeNvidiaSMI(Callback callback): Executes the 'nvidia-smi' command and provides the result via the provided callback.
+ * - executeNvidiaSMI(Callback callback, int delaySecond): Continuously executes the 'nvidia-smi' command at a specified interval (*second*)
+ * and provides the result via the provided callback.
+ */
 public class NvidiaSMIExecutor {
 
+    /**
+     * Continuously executes the 'nvidia-smi' command at a specified interval and provides the result via the provided callback.
+     *
+     * @param callback    The callback interface to handle the results of the 'nvidia-smi' command execution.
+     * @param delaySecond The interval in seconds between each 'nvidia-smi' command execution.
+     */
     public static void executeNvidiaSMI(Callback callback, int delaySecond) {
         new Thread(() -> {
             try {
@@ -33,6 +47,11 @@ public class NvidiaSMIExecutor {
         }).start();
     }
 
+    /**
+     * Executes the 'nvidia-smi' command and provides the result via the provided callback.
+     *
+     * @param callback The callback interface to handle the results of the 'nvidia-smi' command execution.
+     */
     public static void executeNvidiaSMI(Callback callback) {
         try {
             ProcessBuilder processBuilder = new ProcessBuilder("nvidia-smi", "-q", "-x");
@@ -60,9 +79,24 @@ public class NvidiaSMIExecutor {
         }
     }
 
+    /**
+     * The callback interface to handle the results of the 'nvidia-smi' command execution.
+     * Implement this interface to receive the results of 'nvidia-smi' command execution.
+     */
     interface Callback {
+
+        /**
+         * Called when the 'nvidia-smi' command is successfully executed and data is retrieved.
+         *
+         * @param data The JSON data representing the result of the 'nvidia-smi' command execution.
+         */
         void onSuccess(JSONObject data);
 
+        /**
+         * Called when an error occurs during the execution of the 'nvidia-smi' command.
+         *
+         * @param errorMessage The error message describing the reason for the failure.
+         */
         void onError(String errorMessage);
     }
 }
